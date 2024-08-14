5ad1008ed42af15c942806ecd7feb1bf05264d46
Felix <felix.mathiasson99@gmail.com>
2024-08-14 11:30:03 +0200

    tests done

---
+++ tdd-domain-modelling.CSharp.Main/Basket.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tdd_domain_modelling.CSharp.Main
+{
+    public class Basket
+    {
+        Dictionary<string, int> items = new Dictionary<string, int>();
+
+        public bool Add(string product, int price)
+        {
+            if (items.Keys.Contains(product))
+            {
+                return false;
+            }
+            items.Add(product, price);
+            return true;
+        }
+
+
+
+
+        public int Total()
+        {
+            int total = 0;
+            foreach (var key in items)
+            {
+                total += key.Value;
+            }
+            return total;
+        }
+
+
+
+
+
+
+        public Dictionary<string, int> GetItems() {return items;}
+
+       
+    }
+}
---
+++ tdd-domain-modelling.CSharp.Test/BasketTests.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using System.Diagnostics.CodeAnalysis;
+using System.Numerics;
+using tdd_domain_modelling.CSharp.Main;
+
+namespace tdd_domain_modelling.CSharp.Test
+{
+    [TestFixture]
+    public class Basketest
+    {
+        // arrange
+        [TestCase("Cucumber", 22)]
+        public void TestAddToBasket(string product, int price)
+        {
+            //arrange
+            bool expected = true;
+            //act
+            Basket basket = new Basket();
+            if(basket.GetItems().Keys.Contains(product))
+            {
+                expected = false;
+            }
+
+            bool result = basket.Add(product, price);
+            //assert
+            Assert.That(result == expected);
+
+        }
+
+
+
+        [Test]
+        public void TestTotalCost()
+        {
+            //arrange
+
+            int cost = 37;
+            string product = "meat";
+
+
+            //act 
+            Basket basket = new Basket();
+            basket.Add(product, cost);
+
+            Assert.That(basket.Total() == cost);
+        }
+    }
+}
